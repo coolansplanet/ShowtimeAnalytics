8d96c8cd6e9d939d81f7a1ab2330e24b636d8006
Herman Schmidt <hermanschmidt@gmail.com>
2019-01-27 02:02:03 +0000

    build

---
+++ build/precache-manifest.1c331e2a87df2ab7b82c70048436a6a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2f64c1a57f25ede0ee07",
+    "url": "/static/js/main.2f64c1a5.chunk.js"
+  },
+  {
+    "revision": "2bafd1a3af80a9f20e14",
+    "url": "/static/js/1.2bafd1a3.chunk.js"
+  },
+  {
+    "revision": "2f64c1a57f25ede0ee07",
+    "url": "/static/css/main.bfe812eb.chunk.css"
+  },
+  {
+    "revision": "078cae81aaa8374bf593f2334e41e27f",
+    "url": "/index.html"
+  }
+];
